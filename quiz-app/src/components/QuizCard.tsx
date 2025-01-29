fdf8ea434b982a4ca71afbfb8b0d06adc90431d6
Oleksandr Kuzmenko <oleksandr.kuzmenko@corteva.com>
2025-01-29 12:33:03 +0200

    Remove mode_modules

--- quiz-app/src/components/QuizCard.tsx
+++ quiz-app/src/components/QuizCard.tsx
@@ -6,63 +6,107 @@ import {
   Button,
   Typography,
   TextField,
+  Snackbar,
+  SnackbarCloseReason,
+  Alert,
 } from "@mui/material";
 import { useState } from "react";
-import { useTranslationQuestion } from "../hooks/useTranslationQuestion";
 
-export function QuizCard() {
-  const { wordToTranslate, answer, isManualTranslation, options, onNext } =
-    useTranslationQuestion();
+interface QuizCardProps {
+  wordToTranslate: string;
+  answer: string;
+  options?: string[];
+  isManualTranslation: boolean;
+  onNext: () => void;
+}
 
+export function QuizCard({
+  wordToTranslate,
+  answer,
+  options,
+  isManualTranslation,
+  onNext,
+}: QuizCardProps) {
+  const [openErrorMsg, setOpenErrorMsg] = useState(false);
   const [showAnswer, setShowAnswer] = useState(false);
   const [inputValue, setInputValue] = useState("");
-  const onShowAnswer = () => {
-    setShowAnswer(!showAnswer);
+
+  const onShowAnswer = () => setShowAnswer(!showAnswer);
+  const onNextClick = () => {
+    if (answer.toLowerCase() === inputValue.toLowerCase()) {
+      setInputValue("");
+      onNext();
+    } else {
+      setOpenErrorMsg(true);
+    }
   };
 
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h5" component="div"></Typography>
-        <Typography sx={{ color: "text.primary", mb: 1.5 }}>
-          {wordToTranslate}
-        </Typography>
-        <Box>
-          {isManualTranslation && !options ? (
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-          ) : (
-            <div className="flex flex-col justify-center">
-              {options?.map((o) => (
-                <Button key={o} variant="contained" onClick={() => onNext(o)}>
-                  {o}
-                </Button>
-              ))}
-            </div>
-          )}
-        </Box>
-      </CardContent>
-      <CardActions className="flex flex-row justify-center">
-        <Button
-          size="small"
-          variant="contained"
-          color="success"
-          onClick={() => onNext(inputValue)}
-        >
-          Submit
-        </Button>
-        <Button size="small" variant="contained" onClick={onShowAnswer}>
-          Answer
-        </Button>
-        {showAnswer && (
-          <Typography sx={{ color: "text.primary", mb: 1.5 }}>
-            {answer}
+    <>
+      <Card sx={{ maxWidth: "md" }}>
+        <CardContent>
+          <Typography
+            sx={{ color: "text.primary", fontWeight: "bold", fontSize: 40 }}
+          >
+            {wordToTranslate}
           </Typography>
-        )}
-      </CardActions>
-    </Card>
+          {showAnswer && (
+            <Typography sx={{ color: "text.primary", mb: 1.5 }}>
+              {answer}
+            </Typography>
+          )}
+          <Box>
+            {isManualTranslation ? (
+              <TextField
+                variant="outlined"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+            ) : (
+              options?.map((o) => (
+                <Box key={o} className="flex flex-col justify-center m-3">
+                  <Button variant="outlined" onClick={onNextClick}>
+                    {o}
+                  </Button>
+                </Box>
+              ))
+            )}
+          </Box>
+        </CardContent>
+        <CardActions className="flex flex-row justify-center">
+          {isManualTranslation && (
+            <Button
+              size="small"
+              variant="contained"
+              color="success"
+              disabled={!inputValue}
+              onClick={onNextClick}
+            >
+              Submit
+            </Button>
+          )}
+          <Button size="small" variant="contained" onClick={onShowAnswer}>
+            Answer
+          </Button>
+        </CardActions>
+      </Card>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        color="error"
+        open={openErrorMsg}
+        autoHideDuration={2000}
+        onClose={() => setOpenErrorMsg(false)}
+        message="Wrong answer"
+      >
+        <Alert
+          onClose={() => setOpenErrorMsg(false)}
+          severity="error"
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          Wrong answer
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
--- quiz-app/src/components/QuizContainer.tsx
+++ quiz-app/src/components/QuizContainer.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
 import { QuizCard } from "./QuizCard";
+import { getTranslationQuestion } from "../helpers/getTranslationQuestion";
 
 export function QuizContainer() {
-  return <QuizCard />;
+  const [question, setQuestion] = useState(getTranslationQuestion());
+
+  const onNext = () => {
+    setQuestion(getTranslationQuestion());
+  };
+
+  return (
+    <QuizCard
+      wordToTranslate={question.wordToTranslate}
+      answer={question.answer}
+      isManualTranslation={question.isManualTranslation}
+      options={question.isManualTranslation ? undefined : question.options}
+      onNext={onNext}
+    />
+  );
 }
